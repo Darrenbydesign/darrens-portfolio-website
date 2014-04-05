5e69ab5e6a29ee5202797b6d156a0235cf6884de
Darrenbydesign <darrenbydesign@gmail.com>
2014-04-05 15:41:58 -0500

    Convert Portfolio to rails app

---
+++ MySite/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MySite::Application.config.secret_key_base = '0fab40acb14d399bbf2bfbe3fb6d93920758664f91cc94c4ad0cbbd57b71390ad01dfe970d4a1837852536089c1bcd215daa43bf7fc2e002cf51dcdf82c9c1c8'
---
+++ MySite/config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+MySite::Application.config.session_store :cookie_store, key: '_MySite_session'
