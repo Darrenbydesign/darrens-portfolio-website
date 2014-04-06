9dcf5ffc7905e44cfdf3cbb0fef54495929724f8
Darrenbydesign <darrenbydesign@gmail.com>
2014-04-05 19:06:41 -0500

    Modify Secret Token

--- Gemfile
+++ Gemfile
@@ -6,8 +6,14 @@ gem 'rails', '4.0.4'
 gem 'json', '1.8.1'
 gem 'haml' 
 
-group :development do
+group :development, :test do
   gem 'sqlite3', '1.3.8'
+  gem 'rspec-rails', '2.13.1'
+end
+
+group :test do
+  gem 'selenium-webdriver', '2.35.1'
+  gem 'capybara', '2.1.0'
 end
 
 gem 'haml-rails'
@@ -25,4 +31,7 @@ end
 group :production do
   gem 'pg', '0.15.1'
   gem 'rails_12factor', '0.0.2'
+  gem 'unicorn'
 end
+
+
--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -9,4 +9,22 @@
 
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
-MySite::Application.config.secret_key_base = '0fab40acb14d399bbf2bfbe3fb6d93920758664f91cc94c4ad0cbbd57b71390ad01dfe970d4a1837852536089c1bcd215daa43bf7fc2e002cf51dcdf82c9c1c8'
+# MySite::Application.config.secret_key_base = '0fab40acb14d399bbf2bfbe3fb6d93920758664f91cc94c4ad0cbbd57b71390ad01dfe970d4a1837852536089c1bcd215daa43bf7fc2e002cf51dcdf82c9c1c8'
+
+require 'securerandom'
+
+def secure_token
+  token_file = Rails.root.join('.secret')
+  if File.exist?(token_file)
+    # Use the existing token.
+    File.read(token_file).chomp
+  else
+    # Generate a new token and store it in token_file.
+    token = SecureRandom.hex(64)
+    File.write(token_file, token)
+    token
+  end
+end
+
+MySite::Application.config.secret_key_base = secure_token
+
