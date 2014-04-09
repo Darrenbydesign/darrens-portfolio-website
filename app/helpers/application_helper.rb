49f46c778eb31a55a10a331302159c883100d3d6
Darrenbydesign <darrenbydesign@gmail.com>
2014-04-09 16:30:00 -0500

    fix blog image display

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,10 +1,7 @@
 module ApplicationHelper
 	def title
-		base_title = "Darren Sorrels"
-		if @title.nil?
-			base_title
-		else
-			"#{base_title} | #{@title}"
-		end
+		 base_title = "Darren Sorrels"
+
+		 @title.nil? ? base_title : "#{base_title} | #{@title}"
 	end
 end
