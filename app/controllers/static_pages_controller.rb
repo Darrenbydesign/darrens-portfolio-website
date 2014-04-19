f566d46aea7e997343ba9ba7bc8b1a03ae4e1847
Darrenbydesign <darrenbydesign@gmail.com>
2014-04-19 17:00:29 -0500

    updates portfolio images shown in app

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,29 +1,29 @@
 class StaticPagesController < ApplicationController
   def home
      @images = %w(
-		illustration/FOTC_Germaine.jpg
-		illustration/starfox_1.jpg
-		Logos/blk_epiphany.jpg
 		Logos/Flyte_1.jpg
 		Logos/Flyte_2.jpg
 		Logos/Flyte_3.jpg
+		Logos/blk_epiphany.jpg
 		Logos/purls&knit_blk_white_logo.jpg
-		Logos/purls&knit_color_logo.jpg
 		Logos/purls&knit_label.jpg
+		Logos/purls&knit_color_logo.jpg
 		phone_wallpapers/batman.jpg
 		phone_wallpapers/brainiac.jpg
 		phone_wallpapers/flash.jpg
 		phone_wallpapers/superman.jpg
 		posters/almost_maine_poster.jpg
 		posters/Art_of_Murder.jpg
-		posters/Great_expectations_Cover.jpg
-		posters/Moby_Dick_Cover.jpg
 		posters/observations.jpg
 		posters/Regeneration_Poster.jpg
 		posters/TLOZ_type.jpg
 		posters/worldview_lecture.jpg
+		posters/Great_expectations_Cover.jpg
+		posters/Moby_Dick_Cover.jpg
 		weddings/Ruth_Matt_wedding_invite.jpg
 		weddings/Ruth_Matt_wedding_invite_back.jpg
+		illustration/FOTC_Germaine.jpg
+		illustration/starfox_1.jpg
 	)
 	@title = "Home"
 	end
@@ -38,30 +38,30 @@ class StaticPagesController < ApplicationController
 
   def portfolio
     @images = %w(
-		illustration/FOTC_Germaine.jpg
-		illustration/starfox_1.jpg
-		Logos/blk_epiphany.jpg
 		Logos/Flyte_1.jpg
 		Logos/Flyte_2.jpg
 		Logos/Flyte_3.jpg
-		Logos/purls&knit_blk_white_logo.jpg
+		Logos/blk_epiphany.jpg
 		Logos/purls&knit_color_logo.jpg
-		Logos/purls&knit_label.jpg
-		phone_wallpapers/batman.jpg
-		phone_wallpapers/brainiac.jpg
-		phone_wallpapers/flash.jpg
-		phone_wallpapers/superman.jpg
 		posters/almost_maine_poster.jpg
 		posters/Art_of_Murder.jpg
-		posters/Great_expectations_Cover.jpg
-		posters/Moby_Dick_Cover.jpg
 		posters/observations.jpg
 		posters/Regeneration_Poster.jpg
 		posters/TLOZ_type.jpg
 		posters/worldview_lecture.jpg
-		weddings/Ruth_Matt_wedding_invite.jpg
-		weddings/Ruth_Matt_wedding_invite_back.jpg
-	)
+		phone_wallpapers/batman.jpg
+		phone_wallpapers/brainiac.jpg
+		phone_wallpapers/flash.jpg
+		phone_wallpapers/superman.jpg
+		)
+		# Logos/purls&knit_blk_white_logo.jpg
+		# Logos/purls&knit_label.jpg
+		# weddings/Ruth_Matt_wedding_invite.jpg
+		# weddings/Ruth_Matt_wedding_invite_back.jpg
+		# weddings/Ruth_Matt_wedding_invite.jpg
+		# weddings/Ruth_Matt_wedding_invite_back.jpg
+		# illustration/FOTC_Germaine.jpg
+		# illustration/starfox_1.jpg	
 	@title = "Portfolio"
   end
 end
