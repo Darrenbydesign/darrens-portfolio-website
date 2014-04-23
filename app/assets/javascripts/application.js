1e046f0eb06721b2cb05221037b329f76866eeb5
Darrenbydesign <darrenbydesign@gmail.com>
2014-04-23 12:48:56 -0500

    updates the require in application.js to utlize jquery turbo links

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,8 +11,8 @@
 // about supported directives.
 //
 //= require jquery
+//= require jquery.turbolinks
 //= require jquery_ujs
-//=	reqire jquery.turbolinks
 //= require turbolinks
 //= require_self
 //= require_tree .
