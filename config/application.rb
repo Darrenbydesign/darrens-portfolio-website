032bfa1aac9fa893725a1ccbe0d7624458020218
Darrenbydesign <darrenbydesign@gmail.com>
2014-05-01 07:48:17 -0500

    updates the about page to include more information and updates the assets folder to find custom fonts

--- config/application.rb
+++ config/application.rb
@@ -20,5 +20,6 @@ module MySite
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
     config.assets.precompile += %w(*.png *.jpg *.jpeg *.gif *.eof *.wof *.ttf .svg)
+    config.assets.paths << Rails.root.join('app', 'assets', 'fonts')
   end
 end
