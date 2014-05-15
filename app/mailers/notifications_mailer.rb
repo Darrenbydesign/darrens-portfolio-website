47e3beee757a487188f3dcca6eab20781b09d19a
Darrenbydesign <darrenbydesign@gmail.com>
2014-05-14 22:26:02 -0500

    changes configs on mailer again

--- app/mailers/notifications_mailer.rb
+++ app/mailers/notifications_mailer.rb
@@ -1,5 +1,5 @@
 class NotificationsMailer < ActionMailer::Base
-  default :from => :email
+  default :from => "darrenbydesign@gmail.com"
   default :to => "darrenbydesign@gmail.com"
 
   def new_message(message)
--- app/models/message.rb
+++ app/models/message.rb
@@ -15,11 +15,6 @@ class Message
     end
   end
 
-  def deliver
-    return false unless valid?
-    true
-  end
-
   def persisted?
     false
   end
