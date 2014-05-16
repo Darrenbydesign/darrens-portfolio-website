2e13c0041f3e147628ee699778003943f0f763b4
Darrenbydesign <darrenbydesign@gmail.com>
2014-05-15 21:34:55 -0500

    updates the contact notifcation system

--- app/controllers/contact_controller.rb
+++ app/controllers/contact_controller.rb
@@ -1,20 +1,19 @@
 class ContactController < ApplicationController
-  def new
-    @message = Message.new
-  end
-  def create
-    @message = Message.new(params[:message])
-    
-    if @message.valid?
-      NotificationsMailer.new_message(@message).deliver
-      redirect_to(contact_path, :notice => "Thank you, Your message was successfully sent.")
-    else
-      flash.now.alert = "Please fill all fields."
-      render :new
-    end
-  end
-  
-  def contact
-    @title ="Contact"
-  end
+	def new
+		@message = Message.new
+	end
+	def create
+		@message = Message.new(params[:message])
+
+		if @message.valid?
+			NotificationsMailer.new_message(@message).deliver
+			flash[:notice] = "Thank you, Your message was successfully sent."
+			redirect_to(contact_path)
+		else
+			flash.alert = "Oops,looks like there was an error. In some of the fields. 
+			Please check the fields below and try again"
+			render :new
+		end
+	end
+	
 end
--- config/application.rb
+++ config/application.rb
@@ -24,11 +24,10 @@ module MySite
     
     config.action_mailer.delivery_method = :smtp
     config.action_mailer.perform_deliveries = true
-    config.action_mailer.raise_delivery_errors = false
-    config.action_mailer.default :charset => "utf-8"
     config.action_mailer.raise_delivery_errors = true
-    config.action_mailer.default_url_options = { :host => 'http://darrensorrels.herokuapp.com/' }
+    config.action_mailer.default :charset => "utf-8"
 
+    config.action_mailer.default_url_options = { :host => 'http://darrensorrels.herokuapp.com/' }
     config.action_mailer.smtp_settings = {
       address:              'smtp.gmail.com',
       port:                 587,
