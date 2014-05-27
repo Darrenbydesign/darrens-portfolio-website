ccf6e4fc407fb9257a73771435ab536a42805e9e
Darrenbydesign <darrenbydesign@gmail.com>
2014-05-27 01:06:39 -0500

    trying to get fonts to compile

--- config/application.rb
+++ config/application.rb
@@ -19,8 +19,8 @@ module MySite
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
-    config.assets.precompile += %w(*.png *.jpg *.jpeg *.gif *.eof *.wof *.ttf .svg)
     config.assets.paths << Rails.root.join('app', 'assets', 'fonts')
+    config.assets.precompile += %w(*.png *.jpg *.jpeg *.gif *.eof *.wof *.ttf .svg)
     
     config.action_mailer.delivery_method = :smtp
     config.action_mailer.perform_deliveries = true
