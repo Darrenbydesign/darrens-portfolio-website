527a914729efdbae4db03c3c13118451c7c8e8d5
Darrenbydesign <darrenbydesign@gmail.com>
2014-06-04 15:59:09 -0500

    updates the content in the portfolio image descriptions

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -34,11 +34,11 @@ class StaticPagesController < ApplicationController
     Logos/black_epiphany_logo.jpg
     Logos/Empire_fence_logo.jpg
     Logos/purls&knit_logo.jpg
+    posters/observations_poster.jpg
+    posters/worldview_lecture_poster.jpg
     posters/almost_maine_poster.jpg
     posters/Art_of_Murder_poster.jpg
-    posters/observations_poster.jpg
     posters/Regeneration_Poster.jpg
-    posters/worldview_lecture_poster.jpg
     posters/TLOZ_type_poster.jpg
     phone_wallpapers/brainiac_wallpaper.jpg
     phone_wallpapers/superman_wallpaper.jpg
@@ -52,11 +52,11 @@ class StaticPagesController < ApplicationController
       'The Black Epiphany',
       'Empire Fence Co.',
       'Purls & knit',
+      'Observations',
+      'Worldview Lecture Poster',
       'Almost Maine',
       'The Art of Murder',
-      'Observations',
       'Regeneration',
-      'Worldview Lecture Poster',
       'The Legend Of Zelda: Type as Image',
       'DC Brainiac Wallpaper',
       'DC Superman Wallpaper',
@@ -66,21 +66,21 @@ class StaticPagesController < ApplicationController
     ]
     gon.imageText = @imageText = 
     [
-      'Flyte',
-      'The Black Epiphany',
-      'Empire Fence Co.',
-      'Purls & knit',
-      'Almost Maine',
-      'The Art of Murder',
-      'Observations',
-      'Regeneration',
-      'Worldview Lecture Poster',
-      'The Legend Of Zelda: Type as Image',
-      'DC Brainiac Wallpaper',
-      'DC Superman Wallpaper',
-      'DC Flash Wallpaper',
-      'DC Green Lantern Wallpaper',
-      'DC Batman Wallpaper'
+      'The Flyte logo was a concept designed for a Christian based youth group in New York.',
+      'The Black Epiphany logo was designed for a African-American enlightement movement in New Orleans, Lousiana',
+      'Empire Fence Co. is a logo concept designed for a fencing company in Tulsa, Oklahoma',
+      'Purls & knit is a logo designed for a knitting blog of the same name.',
+      'A promotional poster for an Art gallery held at Oral Roberts University',
+      'A promotional poster for a lecture series given on the Oral Roberts University campus',
+      'A promotional poster for the performance of the play Almost, Maine',
+      'A promotional poster for the performance of the play The Art of Murder',
+      'A promotional poster for a dance performance held at Oral Roberts University',
+      'A Legend of Zelda poster concept using typography as image ',
+      'DC comics inspired Wallpaper created for Samsung Galaxy S3',
+      'DC comics inspired Wallpaper created for Samsung Galaxy S3',
+      'DC comics inspired Wallpaper created for Samsung Galaxy S3',
+      'DC comics inspired Wallpaper created for Samsung Galaxy S3',
+      'DC comics inspired Wallpaper created for Samsung Galaxy S3'
     ]
     # gon.hashZip = Hash[@imageName.zip(@imageText)]
     # @imageText = "Information will displayed here describing my work."
