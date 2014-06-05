9a3d8c943e42ca3a2389167b915ce144ba8338d3
Darrenbydesign <darrenbydesign@gmail.com>
2014-06-04 19:01:08 -0500

    fixes order of jquery turbolinks

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,10 +11,9 @@
 // about supported directives.
 //
 //= require jquery
-//= require jquery.turbolinks
 //= require jquery_ujs
-//= require turbolinks
-//= require DBDlightbox
+//= require jquery.turbolinks
 //= require DBDCarousel
-//= require_self
-
+//= require DBDlightbox
+//= require turbolinks
+//= require_tree .
