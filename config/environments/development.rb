74cc0cef174e0736b5f48a93017fc727e307f831
Darrenbydesign <darrenbydesign@gmail.com>
2015-10-31 09:31:58 -0500

    updating the setings in dev and prod environments for action mailer modifies existing contact form

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -33,6 +33,16 @@ MySite::Application.configure do
   config.action_mailer.default :charset => "utf-8"
   
   config.action_mailer.default_url_options = { :host => 'localhost:3000'}
+  ActionMailer::Base.smtp_settings = {
+    :address        => 'smtp.sendgrid.net',
+    :port           => '587',
+    :authentication => :plain,
+    :user_name      => ENV['SENDGRID_USERNAME'],
+    :password       => ENV['SENDGRID_PASSWORD'],
+    :domain         => 'heroku.com',
+    :enable_starttls_auto => true
+  }
+  # Old gmail way of doing this
   # config.action_mailer.smtp_settings = {
   #   address:              'smtp.gmail.com',
   #   port:                 587,
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,7 +78,7 @@ MySite::Application.configure do
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
   config.action_mailer.default_url_options = { host: 'https://darrensorrels.herokuapp.com'}
-  config/action_mailer.delivery_method = :smtp
+  config.action_mailer.delivery_method = :smtp
 
   ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
