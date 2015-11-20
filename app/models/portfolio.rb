b5a3ab7cedf851ce2c3c7629b819d4993e764bc1
Darrenbydesign <darrenbydesign@gmail.com>
2015-11-19 23:36:18 -0600

    removes update to paperclip

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -1,7 +1,6 @@
 class Portfolio < ActiveRecord::Base
   extend FriendlyId
   friendly_id :title, use: :slugged,
-  :image_attributes
   
   has_attached_file :image, styles: { 
     large: "600 x 600>", 
