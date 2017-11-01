60c63a6f07070d2fe56f79cd0999f34ca8f5ec09
Darrenbydesign <darrenbydesign@gmail.com>
2017-11-01 08:04:23 -0500

    small change to show page for portfolios to provide return link and change to s3 creds to remove host name callout

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -41,7 +41,7 @@ MySite::Application.configure do
     :storage => :s3,
     :s3_region => ENV["AWS_S3_REGION"],
     :s3_credentials => {
-      :s3_host_name => ENV["AWS_S3_HOST_NAME"],
+      # :s3_host_name => ENV["AWS_S3_HOST_NAME"],
       :bucket => ENV['S3_BUCKET_NAME'],
       :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
       :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
