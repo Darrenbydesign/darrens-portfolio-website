860a4f0db73be56f3571165aaf6f1b56df139f99
Darrenbydesign <darrenbydesign@gmail.com>
2018-04-18 08:37:51 -0500

    updates the portfolio model to remove sizing styles on images until I can get them resolved

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -2,7 +2,8 @@ class Portfolio < ApplicationRecord
   extend FriendlyId
   friendly_id :title, use: :slugged
   
-  has_attached_file :image, styles: { large: "600x600>", medium: "300x300>", thumb: "100x100>"}
+  has_attached_file :image
+    # styles: { large: "600x600>", medium: "300x300>", thumb: "100x100>"}
 
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
 
