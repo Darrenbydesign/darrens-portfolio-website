faae40ba0054761ed45eeb801fd58a388a8056b0
Darrenbydesign <darrenbydesign@gmail.com>
2018-04-23 07:54:07 -0500

    updates to bundle gems and portfolio show page

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -2,8 +2,11 @@ class Portfolio < ApplicationRecord
   extend FriendlyId
   friendly_id :title, use: :slugged
   
-  has_attached_file :image
-    # styles: { large: "600x600>", medium: "300x300>", thumb: "100x100>"}
+  has_attached_file :image,
+    styles: {
+      medium: ["600x600>"],
+      large: ["900x900>"]
+    }
 
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
 
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -94,7 +94,7 @@ MySite::Application.configure do
   # config/environments/production.rb
   config.paperclip_defaults = {
     :storage => :s3,
-    :s3_region => 'us-east-1',
+    :s3_region => ENV["AWS_REGION"],
     :s3_credentials => {
       :s3_host_name => ENV["AWS_S3_HOST_NAME"],
       :bucket => ENV['S3_BUCKET_NAME'],
