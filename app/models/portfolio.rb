427d8fc7ec0d0d5c8e7cca38674394c81ae51204
Darrenbydesign <darrenbydesign@gmail.com>
2018-04-23 07:56:21 -0500

    updates the model for portfolios to prevent form failure

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -2,11 +2,7 @@ class Portfolio < ApplicationRecord
   extend FriendlyId
   friendly_id :title, use: :slugged
   
-  has_attached_file :image,
-    styles: {
-      medium: ["600x600>"],
-      large: ["900x900>"]
-    }
+  has_attached_file :image
 
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
 
