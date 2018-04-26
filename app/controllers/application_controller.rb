2802e839f7f45f30828bda2e7b27e2246b81b632
Darrenbydesign <darrenbydesign@gmail.com>
2018-04-26 09:08:23 -0500

    adds index to Pages controller to allow for more level of depth in the projects view

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,9 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
+  before_filter :getPageNav
 
   protect_from_forgery with: :exception
-  before_filter :getPageNav
   def getPageNav
     @pageNav = Page.where("is_published = true").order(order: :asc)
   end
