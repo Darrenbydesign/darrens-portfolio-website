a7d7721a8812734fed4fd91fb706253d79d29572
Darrenbydesign <darrenbydesign@gmail.com>
2018-04-28 08:56:40 -0500

    adds logic to show and edit pages for projects

--- app/admin/projects.rb
+++ app/admin/projects.rb
@@ -1,15 +1,33 @@
 ActiveAdmin.register Project do
-# See permitted parameters documentation:
-# https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
-#
-# permit_params :list, :of, :attributes, :on, :model
-#
-# or
-#
-# permit_params do
-#   permitted = [:permitted, :attributes]
-#   permitted << :other if params[:action] == 'create' && current_user.admin?
-#   permitted
-# end
+  # Permit our parameters for the form
+  permit_params :title, :body, :image
 
+  # Determine which parameters whill show on the User Interface
+  index do
+    column :id
+    column :title, :sortable => :title
+    column :body
+    column :image_file_name
+    column :image_file_size
+    column :created_at, :sortable => :created_at
+    actions
+  end
+  show do |s|
+    attributes_table do
+      row :title
+      row :body
+      row :image do
+        project.image? ? image_tag(project.image.url) :content_tag(:span, "No Photo Yet")
+      end
+    end
+  end
+  # Form validation Goes before the form here
+  form :html => { :enctype => "multipart/form-data" } do |f|
+    f.inputs "Project Details" do
+      f.input :title, :label => "Project Title"
+      f.input :body, as: :html_editor, :label => "Project Description"
+      f.input :image, :label =>"Project Header Image", hint: f.project.image? ? image_tag(project.image.url) : content_tag(:span, "Upload JPG/PNG/GIF Image")
+    end
+    f.actions
+  end
 end
--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,15 +1,15 @@
 class ProjectsController < ApplicationController
   def index
-    @projects = Project.all.order("create_at desc")
+    @projects = Project.all.order("created_at desc")
   end
 
   def show
     @project = Project.find(params[:id])
-    @projects = Project.order("create_at desc").limit(4).offset(1)
+    @projects = Project.order("created_at desc").limit(4).offset(1)
   end
   private
     def project_params
-      params.require(:project).permit(:title,:body,:image)
+      params.require(:project).permit(:title, :body, :image)
       
     end
 end
