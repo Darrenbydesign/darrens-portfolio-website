793e7d88d17db381da88ca1bc0af1e34cf8eb80d
Darrenbydesign <darrenbydesign@gmail.com>
2018-04-28 04:16:17 -0500

    fixes typo in projects model for validation of fields

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,6 +1,6 @@
 class Project < ApplicationRecord
-  validates :title, prescence: true, length: { minimum: 4}
-  validates :body, prescence: true
+  validates :title, presence: true, length: { minimum: 4}
+  validates :body, presence: true
   has_attached_file :image, styles: { medium: "300x300>", thumb: "100x100>" }
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
 end
