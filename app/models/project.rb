ab3be32aa18a9a51101fbefa0a7972016a2980c8
Darrenbydesign <darrenbydesign@gmail.com>
2018-04-28 04:07:21 -0500

    adds projects to active admin

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,2 +1,6 @@
 class Project < ApplicationRecord
+  validates :title, prescence: true, length: { minimum: 4}
+  validates :body, prescence: true
+  has_attached_file :image, styles: { medium: "300x300>", thumb: "100x100>" }
+  validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
 end
