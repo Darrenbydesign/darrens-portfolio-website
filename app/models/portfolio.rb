f88dfdfc9a65c90ae00d674ef10a38a731f5f207
Darrenbydesign <darrenbydesign@gmail.com>
2018-04-28 13:16:11 -0500

    removed all the old portfolio info from the app and updates the alerts into a new partial

--- app/models/portfolio.rb
+++
@@ -1,9 +0,0 @@
-class Portfolio < ApplicationRecord
-  extend FriendlyId
-  friendly_id :title, use: :slugged
-  
-  has_attached_file :image
-
-  validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
-
-end
--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,6 @@ MySite::Application.routes.draw do
   get 'projects/show'
 
   devise_for :users
-  resources :portfolios
   resources :contacts, only: [:new, :create]
   resources :projects
   
@@ -15,10 +14,6 @@ MySite::Application.routes.draw do
 
   %w[home about].each do |page|
     get page, controller:'static_pages', action: page
-  end
-
-  %w[portfolio].each do |page|
-    get page, controller: 'portfolios', action: page
   end
 
   # This is a redirect for users if they reach a route that doesn't exist anymore or is not accesible
