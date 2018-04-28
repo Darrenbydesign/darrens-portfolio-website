9dcf81e3ec4c2e463d50e791d773258212d9991a
Darrenbydesign <darrenbydesign@gmail.com>
2018-04-28 09:09:10 -0500

    updates logic of projects to prevent issues with ids and paperclip processing

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -7,9 +7,9 @@ class ProjectsController < ApplicationController
     @project = Project.find(params[:id])
     @projects = Project.order("created_at desc").limit(4).offset(1)
   end
+  
   private
     def project_params
       params.require(:project).permit(:title, :body, :image)
-      
     end
 end
--- app/models/project.rb
+++ app/models/project.rb
@@ -1,7 +1,4 @@
 class Project < ApplicationRecord
-  # Use friendlyID gem
-  extend FriendlyId
-  friendly_id :title
   # validate the values are entered into the form before contin
   validates :title, presence: true, length: {minimum: 4}
   validates :body, presence: true
